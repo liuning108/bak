414786d3514139c396da5c5cd7d419cb7b9d5be5
liuning <122273014@qq.com>
2017-08-21 14:38:35 +0800

    采集机

---
+++ 采集机/machine/i18n/machineMgr.zh.js
@@ -0,0 +1,20 @@
+define({
+	MACHINE_MGR: "采集机管理",
+	MACHINE_NO: "采集机编码",
+	MACHINE_NAME: "采集机名称",
+	MACHINE_IP: "采集机IP地扯",
+	MACHINE_USER: "采集机用户名",
+	DEFAULT_REMAIN: "是否默认处理未分配任务",
+	NEED_CONTINUE_OPER: "请完成当前的操作。",
+	MACHINE_USER_IP_EXIST: "相同的采集机用户及IP地址已存在。",
+	DEFAULT_REMAIN_EXIST: "已存在默认处理未分配任务的采集机。",
+	CONFIRM_DELETE: "确认是否删除该选项？",
+	TASK_SELING: "可选任务",
+	TASK_SELED: "已选任务",
+	TASK_ADD: "添加任务",
+	TASK_DEL: "移除任务",
+	SET_DEFAULT_REMAIN: "置为默认处理未分配任务",
+	CANCEL_DEFAULT_REMAIN: "取消默认处理未分配任务",
+	
+
+});
